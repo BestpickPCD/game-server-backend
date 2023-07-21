d73ae7184acdc4727efa1e5e9bad600e5fd7830f
PCD <pascalpcd@bestpicktechnology.com>
2023-07-21 11:49:42 +0700

    transactions apis

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -7,30 +7,70 @@ export const getTransactions = async (_: Request, res: Response) => {
     const transactions = await prisma.transactions.findMany({
       where: { deletedAt: null },
       orderBy: { createdAt: 'asc' }
-    }); 
+    })
     return res.status(200).json({ message: 'Transactions retrieved successfully', transactions });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Something went wrong', error });
   }
-};
+}
 
 export const addTransaction = async (req: Request, res: Response) => {
   try {
-    const { amount, type, currencyId, userId } = req.body;
-
-    const newTransaction = await prisma.transactions.create({
+    const { amount, action, currencyId, userId } = req.body
+    await prisma.transactions.create({
       data: {
         amount,
-        type,
-        currencyId,
-        userId,
+        action, 
+        user: {
+          connect: { id: userId },
+        },
+        currency: {
+          connect: { id: currencyId },
+        },
       },
     });
 
-    return res.status(201).json({ message: 'Transaction created successfully', transaction: newTransaction });
+    return res.status(201).json({ message: 'Transaction created successfully' })
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Something went wrong', error });
+    return res.status(500).json({ message: 'Something went wrong', error })
   }
-};
+}
+
+export const getBalance = async (req: Request, res: Response) => {
+
+  const userId = parseInt(req.params.userId)
+
+  try {
+    const depositQuery = prisma.transactions.aggregate({
+      _sum: { amount: true, },
+      where: { action: 1, userId, },
+    });
+  
+    const withdrawQuery = prisma.transactions.aggregate({
+      _sum: { amount: true, },
+      where: { action: 2, userId, },
+    });
+  
+    const [depositResult, withdrawResult] = await prisma.$transaction([
+      depositQuery,
+      withdrawQuery,
+    ]);
+  
+    const depositAmount = depositResult._sum?.amount ?? 0;
+    const withdrawAmount = withdrawResult._sum?.amount ?? 0;
+    const balance = Number(depositAmount) - Number(withdrawAmount)
+  
+    return res.status(200).json({ 
+      totalDepositAmount: Number(depositAmount), 
+      totalWithdrawAmount: Number(withdrawAmount), 
+      balance 
+    })
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Unable to fetch balance' });
+  }
+  
+} 
--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -1,10 +1,11 @@
 
 import express from "express";
-import { getTransactions, addTransaction } from "../controllers/transactionController.ts";
+import { getTransactions, addTransaction, getBalance } from "../controllers/transactionController.ts";
 
 const router = express.Router()
 
     router.get("/transactions", getTransactions)
+    router.get("/transaction/:userId", getBalance)
     router.post("/transaction", addTransaction)
 
 export default router

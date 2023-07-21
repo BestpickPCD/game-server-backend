1925ec9007a6a32204de206c80f6d95b8c8863d2
PCD <pascalpcd@bestpicktechnology.com>
2023-07-21 13:11:05 +0700

    remove migration

--- prisma/migrations/20230721030521_alter_type_to_action_in_transactions_table/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `type` on the `transactions` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `transactions` DROP COLUMN `type`,
-    ADD COLUMN `action` INTEGER NULL;

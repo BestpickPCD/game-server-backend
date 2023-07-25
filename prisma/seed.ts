888587ffe1b86fc5da19fa25339e0cc9c7fe4188
nguyen truong <nguyentp@bestpicktechnology.com>
2023-07-25 18:03:37 +0700

    migrate prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,38 +1,45 @@
 import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.roles.createMany({
-      data: [
-        { name: 'admin' },
-        { name: 'user' },
-      ],
-      skipDuplicates: true,
-    });
+  await prisma.roles.createMany({
+    data: [{ name: 'admin' }, { name: 'user' }],
+    skipDuplicates: true
+  });
 
-    await prisma.currencies.create({
-        data: {
-        name: 'Korean Won',
-        code: 'KRW',
-        },
-    });
+  await prisma.currencies.create({
+    data: {
+      name: 'Korean Won',
+      code: 'KRW'
+    }
+  });
 
-    const hashedPassword = await bcrypt.hash('master', 10);
-    
-    await prisma.users.create({
-        data: {
-            name: 'admin',
-            username: 'admin',
-            password: hashedPassword,
-            email: 'admin@master.com',
-            roleId: 1,
-            currencyId: 1,
-        },
-    });
+  const hashedPassword = await bcrypt.hash('master', 10);
+
+  await prisma.users.createMany({
+    data: [
+      {
+        name: 'admin',
+        username: 'admin',
+        password: hashedPassword,
+        email: 'admin@master.com',
+        roleId: 1,
+        currencyId: 1
+      },
+      {
+        name: 'pngyn',
+        username: 'pngyn',
+        password: await bcrypt.hash('nguyen123!', 10),
+        email: 'pngyn@pngyn.com',
+        roleId: 1,
+        currencyId: 1
+      }
+    ]
+  });
 }
- 
+
 main()
   .catch((error) => {
     console.error(error);

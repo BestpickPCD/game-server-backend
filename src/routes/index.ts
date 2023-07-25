2b55163956253e8181f10778381631622737e276
nguyen truong <nguyentp@bestpicktechnology.com>
2023-07-25 11:18:15 +0700

    add swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,41 @@
-import express from "express";
-import currencyRouter from "./currency.js";
-import roleRouter from "./role.js";
-import userRouter from "./user.js";
-import transactionRouter from "./transaction.js";
+import express from 'express';
+import currencyRouter from './currency.js';
+import roleRouter from './role.js';
+import userRouter from './user.js';
+import transactionRouter from './transaction.js';
 
 const router = express.Router();
 
-router.use("", currencyRouter);
-router.use("", roleRouter);
-router.use("", userRouter);
-router.use("", transactionRouter);
+// routes/sample.js (Example route file)
+
+/**
+ * @swagger
+ * tags:
+ *   name: Sample
+ *   description: Sample API endpoints
+ */
+
+/**
+ * @swagger
+ * /api/sample:
+ *   get:
+ *     summary: Get a sample message
+ *     tags: [Sample]
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
+
+router.use('', currencyRouter);
+router.use('', roleRouter);
+router.use('', userRouter);
+router.use('', transactionRouter);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,50 @@ import dotenv from 'dotenv';
 import express from 'express';
 import router from './routes/index.ts';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.1', // YOU NEED THIS
+    info: {
+      title: 'Your API title',
+      version: '1.0.0',
+      description: 'Your API description'
+    },
+    basePath: '/',
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      }
+    },
+    security: [
+      {
+        bearerAuth: []
+      }
+    ]
+  },
+  apis: [`${__dirname}/routes/user.ts`, './routes/user.js']
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);

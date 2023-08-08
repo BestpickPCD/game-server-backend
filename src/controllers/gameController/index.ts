492b9942eab488d5474398dae6f1e9d505a7bb15
PCD <pascalpcd@bestpicktechnology.com>
2023-08-08 10:33:20 +0700

    games

--- src/controllers/gameController/index.ts
+++ src/controllers/gameController/index.ts
@@ -1,6 +1,7 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Vendors } from '@prisma/client';
 import axios from 'axios';
 import { Request, Response } from 'express';
+import { RequestWithUser } from "../../models/customInterfaces";
 const prisma = new PrismaClient();
 
 export const getGameVendors = async (req: Request, res: Response): Promise<any> => {
@@ -43,3 +44,51 @@ export const getGameVendors = async (req: Request, res: Response): Promise<any> 
     return res.status(500).json({ message: 'Something went wrong', error });
   }
 };
+
+export const gameLaunchLink = async (req:RequestWithUser, res:Response): Promise<any> => {
+  try {
+    const queryParams = req.query; 
+    const vendor = queryParams.vendor as string;  
+    // const gameId = queryParams.gameId as string;
+
+    const game = await prisma.vendors.findFirst({
+      where: {
+        name: vendor
+      }
+    }) as Vendors
+
+    const data = {
+      user: req.user,
+      link: game.url
+    }
+    return res.status(200).json(data)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json(error)
+  }
+}
+
+export const getGameUrl = async (req:RequestWithUser, res:Response): Promise<any> => {
+  try {
+    const queryParams = req.query; 
+    const vendor = queryParams.vendor as string;  
+    // const gameId = queryParams.gameId as string;
+
+    const game = await prisma.vendors.findFirst({
+      where: {
+        name: vendor
+      }
+    }) as Vendors
+
+    const data = {
+      user: req.user,
+      link: game.url
+    }
+    return res.status(200).json(data)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json(error)
+  }
+}
+
+
--- src/routes/game.ts
+++ src/routes/game.ts
@@ -1,11 +1,15 @@
 import { 
-    getGameVendors
+    getGameVendors,
+    gameLaunchLink,
+    getGameUrl
   } from "../controllers/gameController/index.ts";
   import express from "express"; 
   
   const router = express.Router();
    
   router.get("/game-list", getGameVendors);
+  router.get("/game-launch-link", gameLaunchLink);
+  router.get("/game-game-url", getGameUrl);
   
   export default router;
   

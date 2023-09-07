99e393a2c4adad472dd26ee71656aa414a760701
PCD <pascalpcd@bestpicktechnology.com>
2023-09-07 15:15:43 +0700

    refactor

--- src/controllers/authenticationController/utilities.ts
+++ src/controllers/authenticationController/utilities.ts
@@ -27,33 +27,26 @@ export const checkUserExist = async (checks: any): Promise<any> => {
 
     let canCreate = false;
     let rawQuery = ``;
-    let select = ``;
     let where = ``;
 
     if (firstName) {
-      select += `, firstname`;
       where += ` AND firstname = '${firstName}'`;
     } else if (lastName) {
-      select += `, lastName`;
       where += ` AND lastName = '${lastName}'`;
     } else if (username) {
-      select += `, username`;
       where += ` AND username = '${username}'`;
     } else if (nickname) {
-      select += `, nickname`;
       where += ` AND nickname = '${nickname}'`;
     } else if (email) {
-      select += `, email`;
       where += ` AND email = '${email}'`;
     } else if (parentAgentId) {
-      select += `, parentAgentId`;
       where += ` AND parentAgentId = '${parentAgentId}'`;
     }
 
     if (type == 'agent') {
-      rawQuery = `SELECT id ${select} FROM Users WHERE 1=1 ${where} `;
+      rawQuery = `SELECT * FROM Users WHERE 1=1 ${where} `;
     } else {
-      rawQuery = `SELECT id ${select} FROM Players WHERE 1=1 ${where} `;
+      rawQuery = `SELECT * FROM Players WHERE 1=1 ${where} `;
     }
     const users = (await prisma.$queryRawUnsafe(`${rawQuery}`)) as any;
     if (users.length == 0) {

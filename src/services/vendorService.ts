4d1f8bb629e1af31c7abb8398fa5ae2dd5394b08
nguyen truong <nguyentp@bestpicktechnology.com>
2023-09-13 14:08:48 +0700

    add get games by playerid

---
+++ src/services/vendorService.ts
@@ -0,0 +1,58 @@
+import { Players, PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export const getAgentByPlayerId = async (
+  playerId: number
+): Promise<Players | null> => {
+  try {
+    const player: Players | null = await prisma.players.findUnique({
+      where: {
+        deletedAt: null,
+        id: playerId,
+        agent: {
+          deletedAt: null
+        }
+      }
+    });
+
+    if (!player) {
+      throw Error('Player not found');
+    }
+    return player;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+export const getGamesByPlayerId = async (
+  playerId: number
+): Promise<
+  ({
+    fetchGames: any;
+    name: string;
+    url: string | null;
+  } | null)[]
+> => {
+  try {
+    const player = await getAgentByPlayerId(playerId);
+    const games = await prisma.agentVendor.findMany({
+      select: {
+        vendor: {
+          select: {
+            fetchGames: true,
+            name: true,
+            url: true
+          }
+        }
+      },
+      where: {
+        agentId: player?.agentId,
+        deletedAt: null
+      }
+    });
+    const data = games.map((game) => game?.vendor);
+    return data;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};

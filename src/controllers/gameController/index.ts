de88e1a13247426774161f58345ca18334c8c845
PCD <pascalpcd@bestpicktechnology.com>
2023-09-19 14:21:51 +0700

    WIP

--- src/controllers/gameController/index.ts
+++ src/controllers/gameController/index.ts
@@ -45,7 +45,6 @@ export const getVendors = async (
 
     return res.status(200).json(rearrangedVendors);
   } catch (error) {
-    console.log(error);
     return res.status(500).json(error);
   }
 };

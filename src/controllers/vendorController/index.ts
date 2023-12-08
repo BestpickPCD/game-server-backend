ab85aabece84fc4fc17eee836d00ad4895e4872d
nguyen truong <nguyentp@bestpicktechnology.com>
2023-12-08 16:54:16 +0700

    update vendor

--- src/controllers/vendorController/index.ts
+++ src/controllers/vendorController/index.ts
@@ -1,169 +1,174 @@
 import { Request, Response } from 'express';
-import { AgentVendor, PrismaClient, Vendors } from "../../config/prisma/generated/base-default/index.js";
-import { RequestWithUser } from "../../models/customInterfaces.ts";
-import { BAD_REQUEST } from '../../core/error.response.ts';
+import {
+  AgentVendor,
+  PrismaClient,
+  Vendors
+} from '../../config/prisma/generated/base-default/index.js';
 import { CREATED, DELETED, OK, UPDATED } from '../../core/success.response.ts';
+import { RequestWithUser } from '../../models/customInterfaces.ts';
 import { message } from '../../utilities/constants/index.ts';
 import { getLogo } from './utilities.ts';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export const getVendors = async (
-    req: RequestWithUser,
-    res: Response
+  req: RequestWithUser,
+  res: Response
 ): Promise<any> => {
-    
-    const vendors = await prisma.vendors.findMany({
+  const vendors = await prisma.vendors.findMany({
+    select: {
+      id: true,
+      name: true,
+      url: true,
+      fetchGames: true,
+      agents: {
         select: {
-            id: true,
-            name: true,
-            url: true,
-            fetchGames: true,
-            agents: {
-                select: {
-                    vendorId: true
-                },
-                where: {
-                    agentId: req.query.agentId ? `${req.query.agentId}` : req.user?.id
-                }
-            }
+          vendorId: true,
+          directUrl: true
         },
         where: {
-            deletedAt: null
+          agentId: req.query.agentId ? `${req.query.agentId}` : req.user?.id
         }
-    });
+      }
+    },
+    where: {
+      deletedAt: null
+    }
+  });
 
-    const rearrangedVendors = vendors.map((vendor) => {
-        const canSee = vendor.agents.length === 1 ? true : false; // Check if there agent is linked to vendor 
-        const { fetchGames, agents, ...data } = {
-            ...vendor,
-            img: getLogo(req.headers.host, vendor.name),
-            gamesTotal: (vendor.fetchGames as [])?.length ?? 0,
-            canSee
-        };
-        return data;
-    });
+  const rearrangedVendors = vendors.map((vendor) => {
+    const canSee = vendor.agents.length === 1 ? true : false; // Check if there agent is linked to vendor
+    const { fetchGames, ...data } = {
+      ...vendor,
+      img: getLogo(req.headers.host, vendor.name),
+      gamesTotal: (vendor.fetchGames as [])?.length ?? 0,
+      canSee
+    };
+    return data;
+  });
 
-    return new OK({ data:rearrangedVendors }).send(res);
+  return new OK({ data: rearrangedVendors }).send(res);
 };
 
-export const getVendorList = async (req:Request, res: Response) => {
-    try { 
-        const { vendorId } = req.query;
-        const filter = typeof vendorId === 'string' ? { id: parseInt(vendorId) } : {};
+export const getVendorList = async (req: Request, res: Response) => {
+  const { vendorId } = req.query;
+  const filter = typeof vendorId === 'string' ? { id: parseInt(vendorId) } : {};
 
-        const vendors = await prisma.vendors.findMany({
-            where: filter
-        }) as Vendors[];
-        const vendorList = vendors.map((vendor) => {
-            const img = getLogo(req.headers.host, vendor.name);
-            const { createdAt, updatedAt, deletedAt, startDate, endDate, keys, ...returnVendor } =  vendor
-            return { ...returnVendor, ...{img: img} } 
-        });
+  const vendors = (await prisma.vendors.findMany({
+    where: filter
+  })) as Vendors[];
+  const vendorList = vendors.map((vendor) => {
+    const img = getLogo(req.headers.host, vendor.name);
+    const {
+      createdAt,
+      updatedAt,
+      deletedAt,
+      startDate,
+      endDate,
+      keys,
+      ...returnVendor
+    } = vendor;
+    return { ...returnVendor, ...{ img: img } };
+  });
 
-        return new OK({ data: vendorList }).send(res);
+  return new OK({ data: vendorList }).send(res);
+};
 
-    } catch (error) {
-        return res.status(500).json({ error: 'Internal Server Error' });
+export const addVendor = async (req: Request, res: Response) => {
+  const { name, url, keys } = req.body;
+  await prisma.vendors.create({
+    data: {
+      name,
+      url,
+      keys
     }
-}
+  });
 
-export const addVendor = async (req:Request, res:Response) => {
+  return new CREATED({ data: message.CREATED }).send(res);
+};
 
-    const { name, url, keys } = req.body;
-
-    await prisma.vendors.create({
-        data: {
-            name, url, keys
-        }
-    })
-
-    return new CREATED({ data: message.CREATED }).send(res);
-}
-
-export const updateVendor = async (req:Request, res:Response) => {
-
-    const { vendorId } = req.params;
-    const { name, keys, url } = req.body;
-    await prisma.vendors.update({
-        where: {
-            id: parseInt(vendorId)
-        },
-        data: {
-            name, url, keys
-        }
-    });
-
-    return new UPDATED({ data: message.UPDATED }).send(res);
-
-}
-
-export const addVendorAgent = async (req:Request, res:Response) => {
-
-    const { agentId, selectedVendors }: { agentId: string, selectedVendors: Vendors[] } = req.body;
-
-    const existingVendors = await prisma.agentVendor.findMany({
-        select: { vendorId: true },
-        where: { agentId }
-    }) as AgentVendor[]
-
-    const existingIds = existingVendors.map(obj => obj.vendorId);
-    const selectedIds = selectedVendors.map(obj => obj.id);
-    const add = selectedIds.filter(id => !existingIds.includes(id));
-    const remove = existingIds.filter(id => !selectedIds.includes(id));
-
-    const toAdd = add.map((vendor) => {
-        return {
-            agentId,
-            vendorId: vendor
-        }
-    });
-    if( add.length > 0) {
-        await prisma.agentVendor.createMany({data:toAdd})
+export const updateVendor = async (req: Request, res: Response) => {
+  const { vendorId } = req.params;
+  const { name, keys, url } = req.body;
+  await prisma.vendors.update({
+    where: {
+      id: parseInt(vendorId)
+    },
+    data: {
+      name,
+      url,
+      keys
     }
-    if( remove.length > 0) {
-        await prisma.agentVendor.deleteMany({
-            where: {
-                agentId,
-                vendorId:{
-                    in: remove ?? []
-                }
-            }
-        })
+  });
+
+  return new UPDATED({ data: message.UPDATED }).send(res);
+};
+
+export const addVendorAgent = async (req: Request, res: Response) => {
+  const {
+    agentId,
+    selectedVendors
+  }: { agentId: string; selectedVendors: Vendors[] } = req.body;
+
+  const existingVendors = (await prisma.agentVendor.findMany({
+    select: { vendorId: true },
+    where: { agentId }
+  })) as AgentVendor[];
+
+  const existingIds = existingVendors.map((obj) => obj.vendorId);
+  const selectedIds = selectedVendors.map((obj) => obj.id);
+  const add = selectedIds.filter((id) => !existingIds.includes(id));
+  const remove = existingIds.filter((id) => !selectedIds.includes(id));
+
+  const toAdd = add.map((vendor) => {
+    return {
+      agentId,
+      vendorId: vendor
+    };
+  });
+  if (add.length > 0) {
+    await prisma.agentVendor.createMany({ data: toAdd });
+  }
+  if (remove.length > 0) {
+    await prisma.agentVendor.deleteMany({
+      where: {
+        agentId,
+        vendorId: {
+          in: remove ?? []
+        }
+      }
+    });
+  }
+
+  return new OK({ message: message.SUCCESS }).send(res);
+};
+
+export const removeVendorAgent = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { vendorId, agentId } = req.body;
+
+  await prisma.agentVendor.delete({
+    where: {
+      id: parseInt(id),
+      agentId,
+      vendorId
     }
+  });
 
-    return new OK({message: message.SUCCESS}).send(res);
+  return new DELETED({ message: message.DELETED }).send(res);
+};
 
-}
+export const updateVendorAgent = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { directUrl } = req.body;
+  await prisma.agentVendor.update({
+    where: {
+      id: parseInt(id)
+    },
+    data: {
+      directUrl
+    }
+  });
 
-export const removeVendorAgent = async (req:Request, res:Response) => {
-    
-    const { id } = req.params;
-    const { vendorId, agentId } = req.body;
-
-    await prisma.agentVendor.delete({
-        where: {
-            id: parseInt(id),
-            agentId,
-            vendorId
-        }
-    });
-
-    return new DELETED({ message: message.DELETED }).send(res);
-
-}
-
-export const updateVendorAgent = async (req:Request, res:Response) => {
-    const { id } = req.params;
-    const { directUrl } = req.body;
-    await prisma.agentVendor.update({
-        where: {
-            id: parseInt(id)
-        },
-        data: {
-            directUrl
-        }
-    });
-
-    return new UPDATED({ message: message.UPDATED }).send(res);
-}
+  return new UPDATED({ message: message.UPDATED }).send(res);
+};

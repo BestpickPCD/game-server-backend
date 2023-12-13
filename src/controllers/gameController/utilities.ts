e7e1a01b51f93a66c7f077bd7ffe937009f6881f
PCD <pascalpcd@bestpicktechnology.com>
2023-12-13 16:12:34 +0700

    provider api

--- src/controllers/gameController/utilities.ts
+++ src/controllers/gameController/utilities.ts
@@ -10,7 +10,7 @@ export const getGameList = async ( vendors: any[] ) => {
         // If directUrl but no url - call through honorlink
         if(directUrl && url) {
             const gameList = await _getDirectURL(url, keys, name);
-            const arrangedData = await _rearrangeData(gameList.data.data, name);
+            const arrangedData = await _rearrangeData(gameList.data.data, name, directUrl);
             list = list.concat(arrangedData);
         } else {
             const gameList = await axios.get(`${process.env.HONORLINK_URL}/api/game-list?vendor=${name}`,{
@@ -18,7 +18,7 @@ export const getGameList = async ( vendors: any[] ) => {
                     Authorization: `Bearer ${process.env.HONORLINK_AGENT_KEY}`
                 }
             }) as any;
-            const arrangedData = await _rearrangeData(gameList.data, name);
+            const arrangedData = await _rearrangeData(gameList.data, name, directUrl);
             list = list.concat(arrangedData);
         }
 
@@ -51,12 +51,13 @@ const _getDirectURL = async (url: string, keys: any, name: string) => {
 
 }
 
-const _rearrangeData = async (data:any[], vendorName: string | null) => {
+const _rearrangeData = async (data:any[], vendorName: string | null, directUrl: boolean) => {
 
     return data.map((datum) => ({
         id: datum.id ?? datum.game_id ?? datum.gameId ?? null,
         name: datum.game_name ?? datum.title ?? datum.name ?? datum.gameName ?? null,
         code: datum.gameCode ?? datum.code ?? datum['game-code'] ?? datum.game_code ?? null,
+        provider: directUrl ? vendorName : 'Honorlink',
         type: datum.type ?? null,
         vendor: datum.vendor ?? vendorName,
         img: datum.thumbnail ?? datum.img_path ?? null,

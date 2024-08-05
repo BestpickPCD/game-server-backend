39231dc768f3bb9b7d446b8a4300928f4801482a
BestpickPCD <pascalpcd@bestpicktechnology.com>
2024-08-05 16:29:12 +0700

    .length

--- src/controllers/gameController/vendors.ts
+++ src/controllers/gameController/vendors.ts
@@ -2,16 +2,20 @@ import axios from 'axios';
 import { v4 as GUIDGen } from 'uuid';
 
 export const __bestpickGameList = async (url: string, keys: any[]) => {
-  const gameUrl = `${url}:6175/games`;
-  const objectKeys = await keys.reduce((acc, cur) => ({ ...acc, ...cur }), {});
-
-  const data = await axios.get(gameUrl, {
-    headers: {
-      ...objectKeys
-    }
-  });
-
-  return data;
+  try {
+    const gameUrl = `${url}:6175/games`;
+    const objectKeys = await keys.reduce((acc, cur) => ({ ...acc, ...cur }), {});
+    
+    const data = await axios.get(gameUrl, {
+      headers: {
+        ...objectKeys
+      }
+    });
+  
+    return data;
+  } catch (error) {
+    console.log(error)
+  }
 };
 
 export const __evolutionGameList = async (url: string, keys: any) => {
@@ -28,29 +32,31 @@ export const __evolutionGameList = async (url: string, keys: any) => {
 export const __pgsoftGameList = async (url: string, keys: any) => {
   try {
     let PGKeys: any
-    keys.forEach((element: any) => {
-      const key = Object.keys(element);
-      PGKeys = { ...{ [`${key}`]: element[`${key}`]}, ...PGKeys }
-    });
-  
-    const { operator_token, secret_key } = PGKeys;
-    const guid = GUIDGen();
-    const data = await axios.post(
-      `${url}/external/Game/v2/Get?trace_id=${guid}`,
-      {
-        operator_token,
-        secret_key,
-        currency: 'KRW',
-        language: 'enus',
-        status: 1
-      },
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+    if(keys && keys?.length ) {
+      keys.forEach((element: any) => {
+        const key = Object.keys(element);
+        PGKeys = { ...{ [`${key}`]: element[`${key}`]}, ...PGKeys }
+      });
+    
+      const { operator_token, secret_key } = PGKeys;
+      const guid = GUIDGen();
+      const data = await axios.post(
+        `${url}/external/Game/v2/Get?trace_id=${guid}`,
+        {
+          operator_token,
+          secret_key,
+          currency: 'KRW',
+          language: 'enus',
+          status: 1
+        },
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
         }
-      }
-    );
-    return data;
+      );
+      return data;
+    }
   } catch (error) {
     console.log(error) 
   }
